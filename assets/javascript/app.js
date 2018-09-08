e168ffc659a3335a8c334e33e5cadaac810fc50b
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-08 15:44:25 -0700

    basic HTML and pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,30 @@
 // ------------------------VARIABLES-----------------------//
 
+//questionOne object
+var questionOne = {
+    question: "What is the name of the game?",
+    option1: "trivia",
+    option2: "basketball",
+    option3: "uno",
+    option4: "hearts"
+}
+
+var questionTwo = {
+    question: "How many of these are left?",
+    option1: "four",
+    option2: "three",
+    option3: "two",
+    option4: "one"
+}
 
 
 // ------------------------FUNCTIONS-----------------------//
+
+// init()
+// page loads, with start button in middle
+
+// when start button is clicked
+//
 
 
 
