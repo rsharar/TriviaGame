96c82c793b80633a44bb5191b46cb750d86c020d
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-08 15:57:59 -0700

    pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,7 @@ var questionOne = {
     option4: "hearts"
 }
 
+//question Two object
 var questionTwo = {
     question: "How many of these are left?",
     option1: "four",
@@ -23,10 +24,17 @@ var questionTwo = {
 // init()
 // page loads, with start button in middle
 
-// when start button is clicked
-//
-
-
+// when start button is clicked -->
+// start timer countdown from 90 seconds
+// randomly select 5 questions
+// create and append new div for questionOne.question 
+// create and append all answer choices as MC options in one div
+// create and append div with 'Submit' button below all questions
+// repeat for all questions
+// if timer = 0 or user clicks submit
+// display questions correct, incorrect, unanswered
+// add 'play again' button
+// DO NOT reload the page, but rather manipulate DOM to reload 5 random new questions
 
 
 // ------------------------EVENT LISTENERES-----------------------//
